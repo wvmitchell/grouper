6cdb37c47be642e2e4fcef049efc7c11cad285ab
Will Mitchell <wvmitchell@gmail.com>
2013-11-30 12:18:14 -0700

    Group implementation overhaul

--- lib/grouper/group.rb
+++ lib/grouper/group.rb
@@ -1,48 +1,66 @@
 module Grouper
   class Group
 
-    attr_reader :members, :teams
+    #attr_reader :members, :teams
 
-    def initialize
-      @members = []
-      @teams = []
-      @unteamed_members = []
-    end
+    #def initialize
+    #  @members = []
+    #  @teams = []
+    #  @unteamed_members = []
+    #end
 
-    def add_members(*people)
-      new_members = [people].flatten
-      new_members.each do |member|
-        @members << member
-        @unteamed_members << member
+    #def add_members(*people)
+    #  new_members = [people].flatten
+    #  new_members.each do |member|
+    #    @members << member
+    #    @unteamed_members << member
+    #  end
+    #end
+
+    #def teams_of(size)
+    #  while unassigned_members?
+    #    can_make_full_team?(size) ? add_team_of(size) : assign_singles
+    #  end
+    #end
+
+    #def add_team_of(size)
+    #  @teams << team_of(size)
+    #end
+
+    #def team_of(size)
+    #  Team.new(@unteamed_members.pop(size))
+    #end
+
+    #def unassigned_members?
+    #  @unteamed_members.length > 0
+    #end
+
+    #def can_make_full_team?(size)
+    #  @unteamed_members.length/size > 0
+    #end
+
+    #def assign_singles
+    #  @unteamed_members.each_with_index do |member, i|
+    #    teams[i].add_member(@unteamed_members.pop)
+    #  end
+    #end
+    #
+
+    attr_accessor :members
+
+    def teams_of(size, ordered_members)
+      unteamed = ordered_members
+      group_size = ordered_members.length
+      num_teams = group_size/size
+      plus_one_teams = group_size%size
+      regular_teams = num_teams - plus_one_teams
+      num_teams.times.collect do |t|
+        t < regular_teams ? team_of(size, unteamed) : team_of(size+1, unteamed)
       end
     end
 
-    def teams_of(size)
-      while unassigned_members?
-        can_make_full_team?(size) ? add_team_of(size) : assign_singles
-      end
-    end
-
-    def add_team_of(size)
-      @teams << team_of(size)
-    end
-
-    def team_of(size)
-      Team.new(@unteamed_members.pop(size))
-    end
-
-    def unassigned_members?
-      @unteamed_members.length > 0
-    end
-
-    def can_make_full_team?(size)
-      @unteamed_members.length/size > 0
-    end
-
-    def assign_singles
-      @unteamed_members.each_with_index do |member, i|
-        teams[i].add_member(@unteamed_members.pop)
-      end
+    def team_of(size, members)
+      members.shift(size)
     end
 
   end
--- test/group_test.rb
+++ test/group_test.rb
@@ -5,70 +5,88 @@ require './lib/grouper'
 
 class GroupTest < MiniTest::Test
 
-  attr_reader :group
+  #attr_reader :group
 
-  def setup
-    people = %w{Will Logan Eric Layla}.collect do |person|
-      Grouper::Member.new(person)
-    end
-    @group = Grouper::Group.new
-    @group.add_members(people)
-  end
+  #def setup
+  #  people = %w{Will Logan Eric Layla}.collect do |person|
+  #    Grouper::Member.new(person)
+  #  end
+  #  @group = Grouper::Group.new
+  #  @group.add_members(people)
+  #end
 
-  def test_a_group_exists
-    assert Grouper::Group
-  end
+  #def test_a_group_exists
+  #  assert Grouper::Group
+  #end
 
-  def test_a_group_has_members
-    assert group.members
-  end
+  #def test_a_group_has_members
+  #  assert group.members
+  #end
 
-  def test_members_always_array
-    person = "Will"
+  #def test_members_always_array
+  #  person = "Will"
+  #  group = Grouper::Group.new
+  #  group.add_members(person)
+  #  assert_kind_of Array, group.members
+  #end
+
+  #def test_a_group_can_choose_a_team
+  #  team = group.team_of(2)
+  #  assert_equal 2, team.size
+  #  team.teammates.each do |person|
+  #    assert_includes group.members, person
+  #  end
+  #end
+
+  #def test_a_group_has_teams
+  #  assert group.teams
+  #end
+
+  #def test_a_group_can_add_a_team
+  #  group.add_team_of(2)
+  #  assert_equal 1, group.teams.length
+  #  assert_kind_of Grouper::Team, group.teams[0]
+  #end
+
+  #def test_size_of_members_does_not_change_when_team_chosen
+  #  num_members = group.members.length
+  #  group.add_team_of(2)
+  #  assert_equal num_members, group.members.length
+  #end
+
+  #def test_teams_within_group_share_no_members
+  #  group.add_team_of(2)
+  #  group.add_team_of(2)
+  #  assert group.teams[0].overlap(group.teams[1]).length == 0
+  #end
+
+  #def test_can_auto_generate_teams
+  #  group.teams_of(2)
+  #  assert_equal 2, group.teams.length
+  #end
+
+  #def test_teams_of_returns_the_teams
+  #  teams = group.teams_of(2)
+  #  assert_equal [['Will', 'Logan'],['Eric', 'Layla']], teams
+  #end
+
+  #def test_can_place_singles_into_plus_one_teams
+  #  group.add_members('Mark', 'Susan', 'Andi')
+  #  group.teams_of(3)
+  #  assert_equal 2, group.teams.length
+  #end
+
+  def test_a_group_has_memebers
     group = Grouper::Group.new
-    group.add_members(person)
-    assert_kind_of Array, group.members
+    group.members = %w{one two three four five six seven eight nine ten}
+    assert_equal %w{one two three four five six seven eight nine ten}, group.members
   end
 
-  def test_a_group_can_choose_a_team
-    team = group.team_of(2)
-    assert_equal 2, team.size
-    team.teammates.each do |person|
-      assert_includes group.members, person
-    end
+  def test_group_can_be_divided_into_teams
+    group = Grouper::Group.new
+    group.members = %w{one two three four five six seven eight nine ten}
+    assert_equal [%w{one two three}, %w{four five six}, %w{seven eight nine ten}], group.teams_of(3,group.members)
   end
 
-  def test_a_group_has_teams
-    assert group.teams
-  end
-
-  def test_a_group_can_add_a_team
-    group.add_team_of(2)
-    assert_equal 1, group.teams.length
-    assert_kind_of Grouper::Team, group.teams[0]
-  end
-
-  def test_size_of_members_does_not_change_when_team_chosen
-    num_members = group.members.length
-    group.add_team_of(2)
-    assert_equal num_members, group.members.length
-  end
-
-  def test_teams_within_group_share_no_members
-    group.add_team_of(2)
-    group.add_team_of(2)
-    assert group.teams[0].overlap(group.teams[1]).length == 0
-  end
-
-  def test_can_auto_generate_teams
-    group.teams_of(2)
-    assert_equal 2, group.teams.length
-  end
-
-  def test_can_place_singles_into_plus_one_teams
-    group.add_members('Mark', 'Susan', 'Andi')
-    group.teams_of(3)
-    assert_equal 2, group.teams.length
-  end
 
 end
